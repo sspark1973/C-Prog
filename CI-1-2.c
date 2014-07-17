540e2e0a099877fd04cd319a000dd76f33c84a10
sspark1973 <sspark1973@gmail.com>
2014-07-17 16:48:40 -0700

    Create CI-1-2.c

---
+++ CI-1-2.c
@@ -0,0 +1,39 @@
+/*
+Write code to reverse a C-Style String. 
+(C-String means that “abcd” is represented as five characters, including the null character.)
+*/
+
+/*
+This is a classic interview question. 
+The only “gotcha” is to try to do it in place, and to be careful for the null character.
+*/
+void reverse(char *str)
+{
+  char *end = str;
+  char tmp;
+  
+  if(str) {
+    while(*end) {
+      ++end;
+    }
+
+    --end;
+  
+    while(str < end) {
+      tmp = *str;
+      *str++ = *end;
+      *end-- = tmp;
+    }
+  }
+}
+  
+
+
+main()
+{
+  char *str = "gotcha";
+  
+  printf("%s \n", str);
+  reverse(str);
+  printf("reverse %s\n", str);
+}
