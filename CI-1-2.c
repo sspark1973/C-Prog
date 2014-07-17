c62c2f40607ce5122acfb1e40043f844e3586805
sspark1973 <sspark1973@gmail.com>
2014-07-17 16:49:26 -0700

    Update CI-1-2.c

--- CI-1-2.c
+++ CI-1-2.c
@@ -3,6 +3,7 @@ Write code to reverse a C-Style String.
 (C-String means that “abcd” is represented as five characters, including the null character.)
 */
 
+#include <stdio.h>
 /*
 This is a classic interview question. 
 The only “gotcha” is to try to do it in place, and to be careful for the null character.
